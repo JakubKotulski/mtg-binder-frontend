f15b6bdf632f8bc3c8f7005f0b3f32f9876bf4a0
Jakub Kotulski <jakub.kotulski@interia.com>
2021-10-21 09:33:37 +0200

    login-page fix

--- src/components/AccountPage/AccountPage.js
+++ src/components/AccountPage/AccountPage.js
@@ -1,16 +1,17 @@
 import "./AccountPage.css";
-import Form from "../Form/Form";
+import LoginForm from "../LoginForm/LoginForm";
+import SignupForm from "../SignupForm/SignupForm";
 
 const AccountPage = () => {
   return (
     <div className="main-box">
       <div className="form-box">
         <h3>Sign up</h3>
-        <Form signupForm={true} />
+        <SignupForm />
       </div>
       <div className="form-box">
         <h3>Login</h3>
-        <Form loginForm={true} />
+        <LoginForm />
       </div>
     </div>
   );
---
+++ src/components/LoginForm/LoginForm.js
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import { Button } from "react-bootstrap";
+import axios from "axios";
+
+const LoginForm = () => {
+  
+
+  const [loginUsername, setLoginUsername] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+
+  
+
+  const onChangeLoginUsername = (event) => {
+    setLoginUsername(event.target.value);
+  };
+
+  const onChangeloginPassword = (event) => {
+    setLoginPassword(event.target.value);
+  };
+
+  
+
+  const login = () => {
+    axios({
+      method: "POST",
+      data: {
+        username: loginUsername,
+        password: loginPassword,
+      },
+      withCredentials: true,
+      url: "http://localhost:4000/login",
+    }).then((res) => {
+      if (res.data === "Successfully Authenticated") {
+        localStorage.setItem("token", res.data);
+      } else {
+        console.log(res.data);
+      }
+    });
+  };
+
+  return (
+    <div>
+      <form className="custom-form">
+        <input placeholder="Username" onChange={onChangeLoginUsername} />
+        <input placeholder="Password" onChange={onChangeloginPassword} />
+        <Button variant="success" onClick={login}>
+          {" "}
+          Login{" "}
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
