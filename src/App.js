e7bf2dfd7be25a4b1de0a8e1109a0a631ebfcbf2
Jakub Kotulski <jakub.kotulski@interia.com>
2021-11-08 18:00:06 +0100

    Removed comment

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import CardUpdatePage from "./components/Pages/CardUpdatePage/CardUpdatePage";
 import ChangeUserDataPage from "./components/Pages/ChangeUserDataPage/ChangeUserDataPage";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-// test
 
 function App() {
   return (
